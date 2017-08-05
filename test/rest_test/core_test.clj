b700b7ab34d3f96896e381ab6d9489f9a68112ac
Jason Felice <jason.m.felice@gmail.com>
2017-08-05 08:03:12 -0500

    Extract `handle` for testing

--- test/rest_test/core_test.clj
+++ test/rest_test/core_test.clj
@@ -3,13 +3,20 @@
             [ring.mock.request :as mock]
             [rest-test.core :as core]))
 
+(defn- handle
+  [& [{:keys [initial-state handler-result]
+       :or {initial-state [],
+            handler-result {}}}]]
+  (let [passed-request (atom nil)
+        handler (core/wrap-state
+                  (fn [request]
+                    (reset! passed-request request)
+                    handler-result)
+                  initial-state)
+        result (handler {})]
+    {:result result
+     :passed-request @passed-request}))
+
 (facts "about the `wrap-state` ring middleware"
   (fact "`wrap-state` passes initial state to the wrapped middleware"
-    (let [passed-request (atom nil)
-          handler (core/wrap-state
-                    (fn [request]
-                      (reset! passed-request request)
-                      {})
-                    42)
-          result (handler {})]
-      @passed-request => (contains {:state 42}))))
+    (:passed-request (handle {:initial-state 42})) => (contains {:state 42})))
